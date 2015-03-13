1cea1094c499e4782b2b502e7376bcef8cae2150
Frode Grimstad Bang <frode@riseup.net>
2015-03-13 13:49:23 +0100

    Databaseoppsett

--- create_tables.php
+++ create_tables.php
@@ -2,7 +2,9 @@
 
 require 'config.php';
 
-$query = "CREATE TABLE room (id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, name VARCHAR(30) NOT NULL, projector ENUM('HDMI', 'VGA'), capacity INT(1) NOT NULL)";
+$query = 'CREATE TABLE room (id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, name VARCHAR(30) NOT NULL, projector ENUM("HDMI", "VGA"), capacity INT(1) NOT NULL);CREATE TABLE user (student_id INT(6) UNSIGNED PRIMARY KEY, name VARCHAR(150) NOT NULL, phone_number INT(8) UNSIGNED NOT NULL, email_address VARCHAR(150) NOT NULL);';//CREATE TABLE booking (id INT(12) UNSIGNED AUTO_INCREMENT PRIMARY KEY, booked_from DATETIME, booked_to DATETIME, FOREIGN KEY (room_id) REFERENCES room (id), FOREIGN KEY (student_id) REFERENCES user (student_id), added TIMESTAMP DEFAULT CURRENT_TIMESTAMP);';
+
+
 
 if (mysqli_query($link, $query)) {
   $database_message = 'things created';

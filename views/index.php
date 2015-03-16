7dd3bbabaf99607c7ff9250f888cc76c45c1b29a
Frode Grimstad Bang <frode@riseup.net>
2015-03-16 13:59:48 +0100

    Datoformatering

--- views/index.php
+++ views/index.php
@@ -5,6 +5,8 @@
 <ul>
   <?php for($i = 0;$i < $bookings_count;$i++): ?>
     <li><p><?php echo $bookings['name']; ?></p></li>
-    <li><?php echo date('d/m H:i', strtotime(str_replace('-','/', $bookings['booked_from']))); ?></li>
+    <li><?php echo date('d/m', strtotime(str_replace('-','/', $bookings['booked_from']))); ?></li>
+    <li><?php echo date('H:i', strtotime(str_replace('-','/', $bookings['booked_from']))); ?></li>
+    <li><?php echo date('H:i', strtotime(str_replace('-','/', $bookings['booked_to']))); ?></li>
   <?php endfor; ?>
 </ul>

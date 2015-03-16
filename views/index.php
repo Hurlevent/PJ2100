b2dc38aa8b9a28c24d61c3e25bfe489bf2ce7583
Frode Grimstad Bang <frode@riseup.net>
2015-03-16 14:13:22 +0100

    Slettet ubrukte filer.

--- views/index.php
+++ views/index.php
@@ -3,10 +3,10 @@
 <h1>Rombooking</h1>
 <p>Du har <?=$this->e($bookings_count)?> booking</p>
 <ul>
-  <?php for($i = 0;$i < $bookings_count;$i++): ?>
+  <?php foreach($bookings as $item): ?>
     <li><p><?php echo $bookings['name']; ?></p></li>
     <li><?php echo date('d/m', strtotime(str_replace('-','/', $bookings['booked_from']))); ?></li>
     <li><?php echo date('H:i', strtotime(str_replace('-','/', $bookings['booked_from']))); ?></li>
     <li><?php echo date('H:i', strtotime(str_replace('-','/', $bookings['booked_to']))); ?></li>
-  <?php endfor; ?>
+  <?php endforeach; ?>
 </ul>

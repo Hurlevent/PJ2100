feaf4fab6b1076a048d9efc5b6d1752395cc9a5f
Hurlevent <eftoli14@student.westerdals.no>
2015-03-19 14:30:48 +0100

    Kopierte over css til css mappen

--- views/layout.php
+++ views/layout.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset='UTF-8'>
     <title><?=$this->e($page_title)?></title>
-    <link rel='stylesheet' type='text/css' href='./html/css/felles.css'>
+    <link rel='stylesheet' type='text/css' href='./css/felles.css'>
     <meta name='viewport' content='width=device-width, initial-scale=1.0'>
   </head>
   <body>

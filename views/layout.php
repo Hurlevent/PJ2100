74999953579d2fb61d8a10df3de01e16dcbeccbf
Frode Grimstad Bang <frode@riseup.net>
2015-03-19 14:49:17 +0100

    Ny meny. Flyttet link til skolens side

--- views/layout.php
+++ views/layout.php
@@ -7,56 +7,35 @@
     <meta name='viewport' content='width=device-width, initial-scale=1.0'>
   </head>
   <body>
-    <!---------------------------------------------------->
-    <!-- Header Start ------------------------------------>
+    <!-- Header Start -->
     <header>
-        <!---------------------------------------------------->
-        <!-- Logo Start -------------------------------------->
+        <!-- Logo Start -->
         
-        <logo>
+        <div class="logo">
             <a href='index.php'><img src="./bilder/WACT_Venstrejustert_svart_rgb_2.svg" alt="logo"/></a>
-        </logo>
-        
-        <!-- Logo End ---------------------------------------->
-        <!---------------------------------------------------->
-        <!-- Navigation Menu --------------------------------->
-        
-        <nav class="font_style_class">
-            <ul>
-                <li class="navbar" >
-                    <a href="http://www.westerdals.no/"> Westerdals Oslo ACT</a></li>
-                <li class="navbar" >
-                    <a href="http://www.westerdals.no/"> Hjelp</a></li>
-            </ul>
-        </nav>
-        
-        <!-- Navigeringsmeny End ----------------------------->
-        <!---------------------------------------------------->
-        <!-- About Start ------------------------------------->
-    
-        <div id="about" class="font_style_class">
-            <ul class="navigation">
-                
-                <?php
-                 if ($user){
-                        echo "<li><img class='icon' src='./bilder/Person_Icon.svg' alt='icon'><a href='/minside.php'>" . $user["full_name"] . "</a></li><li><a href='/logout.php'>Logg ut</a></li>";
-                   } else {
-                       echo "<li><a href='/logg-inn.php'>Logg inn</a></li>";
-                   }
-                ?>
-            </ul>
         </div>
+    
+        <ul class="navigation font_style_class">
+            <?php
+             if ($user){
+                    echo "<li><a href='/minside.php'><img class='icon' src='./bilder/Person_Icon.svg' alt='icon'>" . $user["full_name"] . "</a></li><li><a href='/logout.php'>Logg ut</a></li>";
+               } else {
+                   echo "<li><a href='/logg-inn.php'>Logg inn</a></li>";
+               }
+            ?>
+            <li><a href="/hjelp.php">Hjelp</a></li>
+        </ul>
         
-        <!-- About End --------------------------------------->
-        <!---------------------------------------------------->
+        <!-- About End -->
     </header>
-    <!-- Header End -------------------------------------->
-    <!---------------------------------------------------->
+    <!-- Header End -->
     <div class='content'>
       <?=$this->section('content')?>
     </div>
     <footer>
       <img src="./bilder/WACT_svart_rgb.svg" style="width:20%;">
+      <br>
+      <p><a href="http://www.westerdals.no/"> Westerdals Oslo ACT</a></p>
     </footer>
   </body>
 </html>

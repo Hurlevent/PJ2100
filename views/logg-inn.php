3dc0cc3eed58b56e47807a56e8de2d8d1a41b1d3
Frode Grimstad Bang <frode@riseup.net>
2015-03-19 13:32:08 +0100

    Fikset logg-inn.php konflikt

--- views/logg-inn.php
+++ views/logg-inn.php
@@ -6,7 +6,6 @@
 <div class="login-area">
   <h1>Logg inn</h1>
 
-<<<<<<< HEAD
 <?php
 if (isset($_GET["error"]) && $_GET["error"] == "wronglogin"){
     echo "<p><strong>Feil brukernavn eller passord!</strong></p>";
@@ -29,19 +28,3 @@ if(isset($_GET["error"]) && $_GET["error"] == "nologin"){
       <button class="submit-button" type="submit">Logg inn</button>
     </p>
 </form>
-=======
-  <form class="login-form" action="login.php" method="POST">
-      <p>
-        <label for="username">Brukernavn</label>
-        <input id="username" type="text" name="username" autocomplete="off" />
-      </p>
-      <p>
-        <label for="password">Passord</label>
-        <input id="password" type="password" name="password" />
-      </p>
-      <p>
-        <button class="submit-button" type="submit">Logg inn</button>
-      </p>
-  </form>
-</div>
->>>>>>> origin/master

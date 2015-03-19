30dca03ce2346cc6eb80e536a1fb4cb6cc3e887f
Frode Grimstad Bang <frode@riseup.net>
2015-03-19 15:26:32 +0100

    Slettet ubrukte filer

--- html/php_versions/PJ2100/logOut.php
+++
@@ -1,8 +0,0 @@
-<?php
-require('database.php'); // importerer classen som snakker med databasens
-$database = new DB(); // Oppretter database-objekt
-    session_start(); // Session objekt
-    session_unset(); // fjern alle variabler
-    header("Location: desktop_home.php");
-
-?>
